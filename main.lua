d1e74fe5f1c74c537ee1350e56a087b5c50ed969
Carlo <CarloMehegan@gmail.com>
2019-11-30 23:41:21 -0500

    version 1.0

--- main.lua
+++ main.lua
@@ -22,8 +22,8 @@ function love.load()
   isLeftDown = false
   newClick = false
 
-  energy = 0
-  food = 0
+  energy = 30
+  food = 30
   producingenergy = 0
   producingfood = 0
   axolotls = 0
@@ -40,146 +40,113 @@ function love.load()
   workers = {}
 
   local axox, axoy = 475, 90
+  local cost = 10
   buttons["make_axolotl"] = Button:new(axox, axoy, 100, 50, "axolotl", "10 energy", "single", function()
-    if energy >= 10 then
+    if energy >= cost then
       table.insert(messages, Msg:new("happy birthday!", axox,axoy-10))
       axolotls = axolotls + 1
-      energy = energy - 10
-    elseif energy < 10 then
-      table.insert(messages, Msg:new("insufficient energy!", axox, axoy-15, 0, 0))
+      energy = energy - cost
+    elseif energy < cost then
+      table.insert(messages, Msg:new("need energy!", axox, axoy-15, 0, 0))
     end
   end)
   local fennecx,fennecy = 475,180
-  buttons["make_fennec"] = Button:new(fennecx, fennecy, 100, 50, "fennec fox", "200 energy", "single", function()
-    if energy >= 200 then
+  local cost = 100
+  buttons["make_fennec"] = Button:new(fennecx, fennecy, 100, 50, "fennec fox", "100 energy", "single", function()
+    if energy >= cost then
       table.insert(messages, Msg:new("happy birthday!", fennecx,fennecy-10))
       fennecs = fennecs + 1
-      energy = energy - 200
-    elseif energy < 200 then
-      table.insert(messages, Msg:new("insufficient energy!", fennecx, fennecy-15, 0, 0))
+      energy = energy - cost
+    elseif energy < cost then
+      table.insert(messages, Msg:new("need energy!", fennecx, fennecy-15, 0, 0))
     end
   end)
   local pangox,pangoy = 475,270
-  buttons["make_pangolin"] = Button:new(pangox, pangoy, 100, 50, "pangolin", "1000 energy", "single", function()
-    if energy >= 1000 then
+  local cost = 1100
+  buttons["make_pangolin"] = Button:new(pangox, pangoy, 100, 50, "pangolin", "1K energy", "single", function()
+    if energy >= cost then
       table.insert(messages, Msg:new("happy birthday!", pangox,pangoy-10))
       pangolins = pangolins + 1
-      energy = energy - 1000
-    elseif energy < 1000 then
-      table.insert(messages, Msg:new("insufficient energy!", pangox, pangoy-15, 0, 0))
+      energy = energy - cost
+    elseif energy < cost then
+      table.insert(messages, Msg:new("need energy!", pangox, pangoy-15, 0, 0))
     end
   end)
   local catx,caty = 475,360
-  buttons["make_kitten"] = Button:new(catx, caty, 100, 50, "sand kitten", "5000 energy", "single", function()
-    if energy >= 5000 then
+  local cost = 12000
+  buttons["make_kitten"] = Button:new(catx, caty, 100, 50, "sand kitten", "12K energy", "single", function()
+    if energy >= cost then
       table.insert(messages, Msg:new("happy birthday!", catx,caty-10))
       kittens = kittens + 1
-      energy = energy - 5000
-    elseif energy < 5000 then
-      table.insert(messages, Msg:new("insufficient energy!", catx, caty-15, 0, 0))
+      energy = energy - cost
+    elseif energy < cost then
+      table.insert(messages, Msg:new("need energy!", catx, caty-15, 0, 0))
     end
   end)
   local otterx,ottery = 475,450
-  buttons["make_otter"] = Button:new(otterx, ottery, 100, 50, "sea otter", "30000 energy", "single", function()
-    if energy >= 5000 then
+  local cost = 130000
+  buttons["make_otter"] = Button:new(otterx, ottery, 100, 50, "sea otter", "130K energy", "single", function()
+    if energy >= cost then
       table.insert(messages, Msg:new("happy birthday!", otterx,ottery-10))
       otters = otters + 1
-      energy = energy - 5000
-    elseif energy < 5000 then
-      table.insert(messages, Msg:new("insufficient energy!", otterx, ottery-15, 0, 0))
+      energy = energy - cost
+    elseif energy < cost then
+      table.insert(messages, Msg:new("need energy!", otterx, ottery-15, 0, 0))
     end
   end)
   local sealx,sealy = 475,540
-  buttons["make_seal"] = Button:new(sealx, sealy, 100, 50, "harp seal", "30000 energy", "single", function()
-    if energy >= 5000 then
+  local cost = 1400000 -- 1.4 mil
+  buttons["make_seal"] = Button:new(sealx, sealy, 100, 50, "harp seal", "1.4M energy", "single", function()
+    if energy >= cost then
       table.insert(messages, Msg:new("happy birthday!", sealx,sealy-10))
       seals = seals + 1
-      energy = energy - 5000
-    elseif energy < 5000 then
-      table.insert(messages, Msg:new("insufficient energy!", sealx, sealy-15, 0, 0))
+      energy = energy - cost
+    elseif energy < cost then
+      table.insert(messages, Msg:new("need energy!", sealx, sealy-15, 0, 0))
     end
   end)
   local hippox,hippoy = 475,630
-  buttons["make_hippo"] = Button:new(hippox, hippoy, 100, 50, "hippo", "30000 energy", "single", function()
-    if energy >= 5000 then
+  local cost = 20000000 -- 20 mil
+  buttons["make_hippo"] = Button:new(hippox, hippoy, 100, 50, "hippo", "20M energy", "single", function()
+    if energy >= cost then
       table.insert(messages, Msg:new("happy birthday!", hippox,hippoy-10))
       hippos = hippos + 1
-      energy = energy - 5000
-    elseif energy < 5000 then
-      table.insert(messages, Msg:new("insufficient energy!", hippox, hippoy-15, 0, 0))
+      energy = energy - cost
+    elseif energy < cost then
+      table.insert(messages, Msg:new("need energy!", hippox, hippoy-15, 0, 0))
     end
   end)
   local pandax,panday = 475,720
-  buttons["make_panda"] = Button:new(pandax, panday, 100, 50, "giant panda", "30000 energy", "single", function()
-    if energy >= 5000 then
+  local cost = 330000000 -- 330 mil
+  buttons["make_panda"] = Button:new(pandax, panday, 100, 50, "giant panda", "330M energy", "single", function()
+    if energy >= cost then
       table.insert(messages, Msg:new("happy birthday!", pandax,panday-10))
       pandas = pandas + 1
-      energy = energy - 5000
-    elseif energy < 5000 then
-      table.insert(messages, Msg:new("insufficient energy!", pandax, panday-15, 0, 0))
+      energy = energy - cost
+    elseif energy < cost then
+      table.insert(messages, Msg:new("need energy!", pandax, panday-15, 0, 0))
     end
   end)
 
   local foodx,foody = 330,180
   cooldowns["find_food"] = Cooldown:new(foodx, foody, 100, 50, "find food", 3, function()
-    local found = love.math.random(2, 5)
+    local found = love.math.random(3, 6)
     food = food + found
     table.insert(messages, Msg:new("found "..found.." worms!", foodx, foody-10))
   end)
   local enx,eny = 330,90
   cooldowns["make_energy"] = Cooldown:new(enx, eny, 100, 50, "photosynthesize", 0.2, function()
-    energy = energy + 5
+    energy = energy + 1
     table.insert(messages, Msg:new("energy gained!", enx, eny-10))
   end)
 
 
-
-  worker_costs = {
-    10, 50, 100, 300, 1000, 4500, 9000, 120000
-  }
-  -- worker_animals = {
-  --   "axolotl",
-  --   "fennec",
-  --   "pangolin",
-  --   "kitten",
-  --   "otter",
-  --   "seal",
-  --   "hippo",
-  --   "panda"
-  -- }
-  worker_strength = {
-    10, 50, 100, 300, 1000, 4500, 9000, 120000
-  }
-  -- worker_colors = {
-  --   {1,0,0,0.9},
-  --   {1,0.5,0,0.9},
-  --   {1,1,0,0.9},
-  --   {0,1,0,0.9},
-  --   {0,1,1,0.9},
-  --   {0,0,1,0.9},
-  --   {0.5,0,1,0.9},
-  --   {1,0,1,0.9}
-  -- }
-
-  -- local x = 660
-  -- local x, y = 660, 76 + 34*(i-1)
-  -- buttons[worker_ids[i]] = Button:new(x, y, 15, 15, "+", "", "single", function()
-  --   local cost = worker_costs[i]
-  --   if food >= cost then
-  --     table.insert(messages, Msg:new("new worker!", x+22, y+2))
-  --     table.insert(workers, Worker:new(worker_animals[i], "", worker_strength[i], worker_colors[i]))
-  --     food = food - cost
-  --   elseif food < cost then
-  --     table.insert(messages, Msg:new("need " .. cost .. " food!", x+22, y+2, 0, 0))
-  --   end
-  -- end)
-
+  local cost = 15
   local x, y = 660, 76 + 34*(0)
-  buttons["axo_worker"] = Button:new(x, y, 15, 15, "+", "", "single", function()
-    local cost = 10
+  buttons["axo_worker"] = Button:new(x, y, 15, 15, "+", cost, "single", function()
     if food >= cost and axolotls >= 1 then
       table.insert(messages, Msg:new("new worker!", x+22, y+2))
-      table.insert(workers, Worker:new("axolotl", "", 10, {1,0,0,0.9}))
+      table.insert(workers, Worker:new("axolotl", "", 0.1, {1,0,0,0.9}))
       food = food - cost
       axolotls = axolotls - 1
     elseif food < cost then
@@ -189,12 +156,12 @@ function love.load()
     end
   end)
 
+  local cost = 150
   local x, y = 660, 76 + 34*(1)
-  buttons["fen_worker"] = Button:new(x, y, 15, 15, "+", "", "single", function()
-    local cost = 10
+  buttons["fen_worker"] = Button:new(x, y, 15, 15, "+", cost, "single", function()
     if food >= cost and fennecs >= 1 then
       table.insert(messages, Msg:new("new worker!", x+22, y+2))
-      table.insert(workers, Worker:new("fennec", "", 10, {1,0.5,0,0.9}))
+      table.insert(workers, Worker:new("fennec", "", 1, {1,0.5,0,0.9}))
       food = food - cost
       fennecs = fennecs - 1
     elseif food < cost then
@@ -204,12 +171,12 @@ function love.load()
     end
   end)
 
+  local cost = 1650
   local x, y = 660, 76 + 34*(2)
-  buttons["pan_worker"] = Button:new(x, y, 15, 15, "+", "", "single", function()
-    local cost = 10
+  buttons["pan_worker"] = Button:new(x, y, 15, 15, "+", cost, "single", function()
     if food >= cost and pangolins >= 1 then
       table.insert(messages, Msg:new("new worker!", x+22, y+2))
-      table.insert(workers, Worker:new("pangolin", "", 10, {1,1,0,0.9}))
+      table.insert(workers, Worker:new("pangolin", "", 8, {1,1,0,0.9}))
       food = food - cost
       pangolins = pangolins - 1
     elseif food < cost then
@@ -219,12 +186,12 @@ function love.load()
     end
   end)
 
+  local cost = 1800
   local x, y = 660, 76 + 34*(3)
-  buttons["cat_worker"] = Button:new(x, y, 15, 15, "+", "", "single", function()
-    local cost = 10
+  buttons["cat_worker"] = Button:new(x, y, 15, 15, "+", cost, "single", function()
     if food >= cost and kittens >= 1 then
       table.insert(messages, Msg:new("new worker!", x+22, y+2))
-      table.insert(workers, Worker:new("kitten", "", 10, {0,1,0,0.9}))
+      table.insert(workers, Worker:new("kitten", "", 47, {0,1,0,0.9}))
       food = food - cost
       kittens = kittens - 1
     elseif food < cost then
@@ -234,12 +201,12 @@ function love.load()
     end
   end)
 
+  local cost = 195000
   local x, y = 660, 76 + 34*(4)
-  buttons["ott_worker"] = Button:new(x, y, 15, 15, "+", "", "single", function()
-    local cost = 10
+  buttons["ott_worker"] = Button:new(x, y, 15, 15, "+", cost, "single", function()
     if food >= cost and otters >= 1 then
       table.insert(messages, Msg:new("new worker!", x+22, y+2))
-      table.insert(workers, Worker:new("otter", "", 10, {0,1,1,0.9}))
+      table.insert(workers, Worker:new("otter", "", 260, {0,1,1,0.9}))
       food = food - cost
       otters = otters - 1
     elseif food < cost then
@@ -249,12 +216,12 @@ function love.load()
     end
   end)
 
+  local cost = 2.1 * 1000000
   local x, y = 660, 76 + 34*(5)
-  buttons["sea_worker"] = Button:new(x, y, 15, 15, "+", "", "single", function()
-    local cost = 10
+  buttons["sea_worker"] = Button:new(x, y, 15, 15, "+", cost, "single", function()
     if food >= cost and seals >= 1 then
       table.insert(messages, Msg:new("new worker!", x+22, y+2))
-      table.insert(workers, Worker:new("seal", "", 10, {0,0,1,0.9}))
+      table.insert(workers, Worker:new("seal", "", 1400, {0,0,1,0.9}))
       food = food - cost
       seals = seals - 1
     elseif food < cost then
@@ -264,12 +231,13 @@ function love.load()
     end
   end)
 
+  local cost = 30 * 1000000
   local x, y = 660, 76 + 34*(6)
-  buttons["hip_worker"] = Button:new(x, y, 15, 15, "+", "", "single", function()
-    local cost = 10
+  buttons["hip_worker"] = Button:new(x, y, 15, 15, "+", cost, "single", function()
+
     if food >= cost and hippos >= 1 then
       table.insert(messages, Msg:new("new worker!", x+22, y+2))
-      table.insert(workers, Worker:new("hippo", "", 10, {0.5,0,1,0.9}))
+      table.insert(workers, Worker:new("hippo", "", 7800, {0.5,0,1,0.9}))
       food = food - cost
       hippos = hippos - 1
     elseif food < cost then
@@ -279,12 +247,12 @@ function love.load()
     end
   end)
 
+  local cost = 495 * 1000000
   local x, y = 660, 76 + 34*(7)
-  buttons["gia_worker"] = Button:new(x, y, 15, 15, "+", "", "single", function()
-    local cost = 10
+  buttons["gia_worker"] = Button:new(x, y, 15, 15, "+", cost, "single", function()
     if food >= cost and pandas >= 1 then
       table.insert(messages, Msg:new("new worker!", x+22, y+2))
-      table.insert(workers, Worker:new("panda", "", 10, {1,0,1,0.9}))
+      table.insert(workers, Worker:new("panda", "", 44000, {1,0,1,0.9}))
       food = food - cost
       pandas = pandas - 1
     elseif food < cost then
@@ -319,8 +287,8 @@ function love.update(dt)
       producingenergy = producingenergy + v.strength
       energy = energy + (v.strength * dt)
     elseif v.job == "food" then
-      producingfood = producingfood + v.strength
-      food = food + (v.strength * dt)
+      producingfood = producingfood + v.strength/4
+      food = food + (v.strength/4 * dt)
     end
   end
 
